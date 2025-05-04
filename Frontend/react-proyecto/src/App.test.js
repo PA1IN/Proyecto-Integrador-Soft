077debc235fc9e8d989b1e3eecd8109ba4566ce2
PA1IN <d.ramosguerra018@gmail.com>
2025-05-04 19:51:01 -0400

    .

--- Frontend/react-proyecto/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders login form', () => {
-  render(<App />);
-  const loginButton = screen.getByText(/INICIAR SESION/i);
-  expect(loginButton).toBeInTheDocument();
-});
