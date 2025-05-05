7d0cd02c95a7720f3efb731124abeac9adee41be
PA1IN <d.ramosguerra018@gmail.com>
2025-05-05 00:15:35 -0400

    .

--- Frontend/front-proyecto/src/App.tsx
+++ Frontend/front-proyecto/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter} from 'react-router-dom';
+import {AppRoutes} from './routes/AppRoutes';
+import { ProveedorAuth } from './context/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 
+const clienteQuery = new QueryClient();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(
+    <ProveedorAuth>
+      <QueryClientProvider client = {clienteQuery}>
+        <BrowserRouter>
+          <div className='App'>
+            <AppRoutes/>
+          </div>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ProveedorAuth>
+  ) 
+};
 
 export default App;
