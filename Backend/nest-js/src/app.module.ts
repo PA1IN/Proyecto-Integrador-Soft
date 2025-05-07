adf3c5d9a6594611be58148c96121bfe96b832c4
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2025-05-06 23:01:26 -0400

    Backend v3(detalles por corregir)

--- Backend/nest-js/src/app.module.ts
+++ Backend/nest-js/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ 
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'admin',
+    password: '12345',
+    database: 'db_crud',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+  ]
 })
 export class AppModule {}
--- Backend/nest-js/src/asignaturas/asignaturas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AsignaturasService } from './asignaturas.service';
-import { AsignaturasController } from './asignaturas.controller';
-
-@Module({
-  controllers: [AsignaturasController],
-  providers: [AsignaturasService],
-})
-export class AsignaturasModule {}
