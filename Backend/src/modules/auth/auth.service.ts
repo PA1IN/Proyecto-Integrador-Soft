8743ebbc2d3dfb4082b36eda9aa08746615244bb
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2025-05-09 17:12:44 -0400

    aqui

--- Backend/src/modules/auth/auth.service.ts
+++ Backend/src/modules/auth/auth.service.ts
@@ -25,7 +25,12 @@ export class AuthService {
         const payload = { rut: user.rut, name: user.name };
         const  token = await this.jwtService.signAsync(payload);
         return {
-            token
+            token,
+            user: {
+                rut: user.rut,
+                name: user.name,
+                correo: user.correo,
+            },
         };
 
 
