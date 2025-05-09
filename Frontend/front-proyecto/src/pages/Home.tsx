b6228120f47b4fb76c95b04bcc6c01d0920621d6
BarcobasureroBG <benjamin.garces@alumnos.ucn.cl>
2025-05-09 19:29:10 -0400

    arreglando tema del token

--- Frontend/front-proyecto/src/pages/Home.tsx
+++ Frontend/front-proyecto/src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useUserProfile } from '../hooks/useUserProfile';
+//import { useUserProfile } from '../hooks/useUserProfile';
 import {useNavigate} from "react-router-dom";
 import { useAuth } from '../context/AuthContext';
 
 export const Home = () => {
     const {token, setToken} = useAuth();
     const navigate = useNavigate();
-    const { data: user, isLoading: cargauser, isError} = useUserProfile();
+    //const { data: user, isLoading: cargauser, isError} = useUserProfile();
     
 
     if(!token)
@@ -22,21 +22,21 @@ export const Home = () => {
     }
 
 
-    if(cargauser)
-    {
-        return <div> Cargando... </div>;
-    }
+    //if(cargauser)
+    //{
+        //return <div> Cargando... </div>;
+    //}
     
-    if(isError)
-    {
-        setToken(null);
-        navigate('/login');
-        return null;
-    }
+    //if(isError)
+    //{
+        //setToken(null);
+        //navigate('/login');
+        //return null;
+    //}
 
     return (
     <div> 
-        <h2> Bienvenido: {user?.Name}, tu correo es: {user?.Email} </h2>
+        <h2> Token: {token} </h2>
         <button onClick={()=> navigate('/Calendar')}>Ver las asignaturas definidas</button>
         <button onClick={logout}>Cerrar la sesion 🤑</button>
     </div>
