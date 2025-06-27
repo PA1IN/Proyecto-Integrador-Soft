0b9c344c5fa62a9806f9b47f6baa90e718269da5
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2025-06-27 10:58:49 -0400

    a

--- Backend/src/modules/sala/sala.controller.ts
+++ Backend/src/modules/sala/sala.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SalaService } from './sala.service';
 import { crearSalaDto } from './dto/crear-sala.dto';
 
 @Controller('sala')
 export class SalaController {
     constructor(
-        private readonly salaService: SalaService, // Replace 'any' with the actual service type
+        private readonly salaService: SalaService, 
     ) {
         
     }
@@ -33,7 +33,7 @@ export class SalaController {
     }
 
     @Patch('/:id')
-    eliminarSala(@Body('id') id: number) {
+    eliminarSala(@Param('id') id: number) {
         return this.salaService.eliminarSala(id); // Call the service method to delete a room by ID
     }
 
